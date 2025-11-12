0041bab7693f371f5ca932bc013483c7ec035065
pris-matic <deocadizjranthony@gmail.com>
2025-11-12 22:03:28 +0800

    registered models, and added inlines

--- buildings_and_venues/admin.py
+++ buildings_and_venues/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Building, Venue, Amenity, Venue_amenity
 
-# Register your models here.
+class VenueInline(admin.TabularInline):
+    model = Venue
+    extra = 1
+    fields = ['name', 'type', 'capacity', 'under_renovation']
+    show_change_link = True
+
+class VenueAmenityInline(admin.TabularInline):
+    model = Venue_amenity
+    extra = 1
+    autocomplete_fields = ['amenity']
+
+class BuildingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'street', 'district', 'city')
+    search_fields = ('name', 'city', 'district')
+    inlines = [VenueInline]
+
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'capacity', 'venue_floor_area', 'under_renovation', 'building')
+    list_filter = ('under_renovation', 'type', 'building')
+    search_fields = ('name', 'type', 'building__name')
+    inlines = [VenueAmenityInline]
+
+class AmenityAdmin(admin.ModelAdmin):
+    list_display = ('type', 'description', 'venue')
+    search_fields = ('type', 'venue__name')
+
+# TODO: do you think we should not register this, and just use inlines instead?
+class VenueAmenityAdmin(admin.ModelAdmin):
+    list_display = ('venue', 'amenity')
+    search_fields = ('venue__name', 'amenity__type')
+
+admin.site.register(Building, BuildingAdmin)
+admin.site.register(Venue, VenueAdmin)
+admin.site.register(Amenity, AmenityAdmin)
+admin.site.register(Venue_amenity, VenueAmenityAdmin)
