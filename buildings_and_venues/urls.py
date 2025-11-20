4665a8d1abd14f4a19d39b86c85045712908b335
Bon Dela Cruz <justinbondc322@gmail.com>
2025-11-20 19:15:25 +0800

    Created dashboard to be used for viewing all buildings and their respective venues.

--- buildings_and_venues/urls.py
+++ buildings_and_venues/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage, admin_dashboard
 
 urlpatterns = [
     path('home/', homepage, name='homepage'),
+    path('home/dashboard/', admin_dashboard, name='admin_dashboard')
 ]
 
 app_name = 'buildings_and_venues'
--- buildings_and_venues/views.py
+++ buildings_and_venues/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import District, Building, Venue, Amenity, VenueAmenity
+from .forms import DistrictForm, BuildingForm, VenueForm, AmenityForm, VenueAmenityForm
 
 def homepage(request):
     return render(request,'buildings_and_venues/homepage.html')
+
+def admin_dashboard(request):
+    buildings = Building.objects.prefetch_related('venues').all()
+        
+    return render(request, 'buildings_and_venues/admin_dashboard.html', {
+        'buildings': buildings
+    })
